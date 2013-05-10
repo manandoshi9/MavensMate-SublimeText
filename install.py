34673b52dada7408cba7f022ad28b3e701818a70
manandoshi9 <manan.doshi9@gmail.com>
2013-05-10 18:19:36 -0400

    updated isntall script

--- install.py
+++ install.py
@@ -1,6 +1,7 @@
 try:
     import os
     import sys
+    import platform
     import shutil
     import argparse
     import pipes
@@ -9,28 +10,27 @@ try:
         "darwin" : os.path.expanduser("~/Library/Application Support/Sublime Text 2/Packages/MavensMate"),
         "win32"  : "",
         "cygwin" : "",
-        "linux2" : ""
+        "linux2" : os.path.expanduser("~/.config/sublime-text-2/Packages/MavensMate")
     }
 
     user_settings_path = {
         "darwin" : os.path.expanduser("~/Library/Application Support/Sublime Text 2/Packages/User"),
         "win32"  : "",
         "cygwin" : "",
-        "linux2" : ""
+        "linux2" : os.path.expanduser("~/.config/sublime-text-2/Packages/User")
     }
 
-    platform            = sys.platform
-    install_path        = install_paths[platform]
-    user_settings_path  = user_settings_path[platform]
+    sys_platform        = sys.platform
+    install_path        = install_paths[sys_platform]
+    user_settings_path  = user_settings_path[sys_platform]
     branch              = None
-    git_url             = pipes.quote('git://github.com/joeferraro/MavensMate-SublimeText.git')
+    git_url             = pipes.quote('git://github.com/manandoshi9/MavensMate-SublimeText.git')
 
     def install_from_source():
+        branch = '2.0'
         os.system("git clone {0} {1}".format(git_url, pipes.quote(install_path)))
-        if branch != None and branch != '':
-            #2.0 is beta
-            os.chdir(install_path)
-            os.system("git checkout -b {0} origin/{0}".format(pipes.quote(branch)))
+        os.chdir(install_path)
+        os.system("git checkout -b {0} origin/{0}".format(pipes.quote(branch)))
 
     def install_user_settings():
         if os.path.isfile(user_settings_path+"/mavensmate.sublime-settings") == False:
@@ -39,12 +39,21 @@ try:
                 pipes.quote(user_settings_path)
             ))
 
+    def delete_32_or_64():
+        platform_arch = platform.architecture()[0]
+        if ( platform_arch == '32bit' ):
+            os.system ( "rm -rf {0}".format(pipes.quote(install_path + "/support/linux64" ) ) )
+        else:
+            os.system ( "rm -rf {0}".format(pipes.quote(install_path + "/support/linux32" ) ) )
+
     def uninstall():
         os.system("rm -rf {0}".format(pipes.quote(install_path)))
 
     def install():
         uninstall()
         install_from_source()
+        if ( sys_platform == 'linux2' ):
+            delete_32_or_64()
         install_user_settings()
 
     if __name__ == '__main__':
