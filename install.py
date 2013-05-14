e04f20afe90fa8912b6702a6e158658621e26a63
manandoshi9 <manan.doshi9@gmail.com>
2013-05-13 23:49:14 -0300

    Update install.py

--- install.py
+++ install.py
@@ -29,8 +29,6 @@ try:
     def install_from_source():
         branch = '2.0'
         os.system("git clone {0} {1}".format(git_url, pipes.quote(install_path)))
-        os.chdir(install_path)
-        os.system("git checkout -b {0} origin/{0}".format(pipes.quote(branch)))
 
     def install_user_settings():
         if os.path.isfile(user_settings_path+"/mavensmate.sublime-settings") == False:
