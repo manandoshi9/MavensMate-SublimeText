bbed91f761b90949bde46d891f27540bc25fe950
manandoshi9 <manan.doshi9@gmail.com>
2013-06-01 20:54:45 -0400

    freezing all the new files

--- support/linux64/mm/lib/sforce/base.py
+++ support/linux64/mm/lib/sforce/base.py
@@ -19,9 +19,11 @@ import sys
 import os.path
 import re
 import lib.config as config
+import lib.mm_util as mm_util
 from suds.plugin import MessagePlugin
 from suds.client import Client
 from suds.xsd.doctor import Import, ImportDoctor
+from suds.transport.http import HttpTransport as SudsHttpTransport
 
 try:
   # suds 0.3.8 and prior
@@ -117,7 +119,7 @@ class SforceBaseClient(object):
     if 'retxml' in kwargs:
       xml_response = kwargs['retxml']
 
-    self._sforce = Client(wsdl, cache = cache, plugins=[PrunePlugin()], retxml=xml_response)
+    self._sforce = Client(wsdl, cache=cache, plugins=[PrunePlugin()], retxml=xml_response, transport=WellBehavedHttpTransport())
     #temp = str(self._sforce)
     #print temp
     if 'server_url' in kwargs:
@@ -130,7 +132,7 @@ class SforceBaseClient(object):
         api_version = kwargs['apiVersion']
       self._apiVersion = api_version
     else:
-      self._apiVersion = 26.0
+      self._apiVersion = 27.0
 
     # Set HTTP headers
     headers = {'User-Agent': 'Salesforce/' + self._product + '/' + '.'.join(str(x) for x in self._version)}
@@ -139,7 +141,7 @@ class SforceBaseClient(object):
     # 'Accept-Encoding': 'gzip, deflate'
 
     if kwargs.has_key('environment') and 'sandbox' in kwargs['environment']:
-      self._setEndpoint("https://test.salesforce.com/services/Soap/u/25.0")
+      self._setEndpoint("https://test.salesforce.com/services/Soap/u/"+mm_util.SFDC_API_VERSION)
     
 
     self._sforce.set_options(headers = headers)
@@ -157,6 +159,9 @@ Salesforce will use HTTPS.')
 
     if kwargs.has_key('password'):
       self._sforce.set_options(password = kwargs['password'])
+
+    #set the timeout from a setting
+    self._sforce.set_options(timeout=config.connection.get_plugin_client_setting('mm_timeout', 3600))
 
   # Toolkit-specific methods
 
@@ -634,3 +639,28 @@ Salesforce will use HTTPS.')
 
   def setUserTerritoryDeleteHeader(self, header):
     self._userTerritoryDeleteHeader = header
+
+class WellBehavedHttpTransport(SudsHttpTransport): 
+    """HttpTransport which properly obeys the ``*_proxy`` environment variables.""" 
+
+    def u2handlers(self): 
+        """Return a list of specific handlers to add. 
+
+        The urllib2 logic regarding ``build_opener(*handlers)`` is: 
+
+        - It has a list of default handlers to use 
+
+        - If a subclass or an instance of one of those default handlers is given 
+            in ``*handlers``, it overrides the default one. 
+
+        Suds uses a custom {'protocol': 'proxy'} mapping in self.proxy, and adds 
+        a ProxyHandler(self.proxy) to that list of handlers. 
+        This overrides the default behaviour of urllib2, which would otherwise 
+        use the system configuration (environment variables on Linux, System 
+        Configuration on Mac OS, ...) to determine which proxies to use for 
+        the current protocol, and when not to use a proxy (no_proxy). 
+
+        Thus, passing an empty list will use the default ProxyHandler which 
+        behaves correctly. 
+        """ 
+        return []
